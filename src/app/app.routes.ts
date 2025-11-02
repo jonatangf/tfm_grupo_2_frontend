c4b2a43911bbd62d4ef255908b24a1f53be39c38
gentzane99 <gentzaneop@gmail.com>
2025-11-02 16:28:41 +0100

    Pages and services first structure created
    pages/
     │    ├── home/
     │    ├── login/
     │    ├── register/
     │    ├── profile/
     │    ├── trips/
     │    │    ├── tripsList/
     │    │    ├── tripDetail/
     │    │    ├── tripForm/

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { TripListComponent } from './pages/trips/trip-list/trip-list.component';
+import { TripDetailComponent } from './pages/trips/trip-detail/trip-detail.component';
+import { TripFormComponent } from './pages/trips/trip-form/trip-form.component';
+export const routes: Routes = [
+    {path: "", pathMatch: 'full', redirectTo: 'home'},
+    {path: "home", component: HomeComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "profile", component: ProfileComponent},
+    {path: "trips", component: TripListComponent},
+    {path: "trips/:idTrip", component: TripDetailComponent},
+    {path: "newTrip", component: TripFormComponent},
+    {path: "updateTrip/:idTrip", component: TripDetailComponent},
+    {path: "**", redirectTo:'home'}
+];
