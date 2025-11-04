a7d02ce26daaf89c86a72b7afb7cc5c89f2b783e
Juan DELL <juancarrascoguerrero@gmail.com>
2025-11-04 16:11:15 +0100

    login-guard implementada en routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,22 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { TripListComponent } from './pages/trips/trip-list/trip-list.component';
 import { TripDetailComponent } from './pages/trips/trip-detail/trip-detail.component';
 import { TripFormComponent } from './pages/trips/trip-form/trip-form.component';
+import { loginGuard } from './guards/login-guard';
 
 export const routes: Routes = [
-    {path: "", pathMatch: 'full', redirectTo: 'home'},
-    {path: "home", component: HomeComponent},
-    {path: "login", component: LoginComponent},
-    {path: "register", component: RegisterComponent},
-    //Juan-------- Guard = true --> Acceso ¿dashboard o profile?
-    {path: "profile", component: ProfileComponent},   
-    {path: "trips", component: TripListComponent},
-    {path: "trips/:idTrip", component: TripDetailComponent},
-    {path: "newTrip", component: TripFormComponent},
-    {path: "updateTrip/:idTrip", component: TripDetailComponent},
-    {path: "**", redirectTo:'home'}
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  //Juan-------- Guard = true --> Acceso ¿dashboard o profile como pagina inicial?
+  {path: 'profile', component: ProfileComponent, canActivate: [loginGuard],
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'profile' },
+      { path: 'trips', component: TripListComponent },
+      { path: 'trips/:idTrip', component: TripDetailComponent },
+      { path: 'newTrip', component: TripFormComponent },
+      { path: 'updateTrip/:idTrip', component: TripDetailComponent },
+    ],
+  },
+  { path: '**', redirectTo: 'home' },
 ];
