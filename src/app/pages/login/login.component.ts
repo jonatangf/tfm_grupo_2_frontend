f9fae3cac11d76be3bd279bb30b5e51005689d7a
JuanCarrascoGuerrero <106087948+JuanCarrascoGuerrero@users.noreply.github.com>
2025-11-20 06:50:21 +0100

    Home-login-register debugged

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,48 +6,33 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  encapsulation: ViewEncapsulation.None  //FormGroup de angular trae estilo por encasulapcion (quitamos)
+  encapsulation: ViewEncapsulation.None, //FormGroup de angular trae estilo por encasulapcion (quitamos)
 })
 export class LoginComponent {
-@Input() register! : boolean;
-@Output() submitted = new EventEmitter<any>();
-userForm! : FormGroup;
+  @Input() register!: boolean;
+  @Output() submitted = new EventEmitter<any>();
+  userForm!: FormGroup;
 
-constructor(){
-  this.userForm = new FormGroup({
-    username: new FormControl(""),
-    email: new FormControl("",[
-      Validators.required,
-      Validators.pattern(/\S+\@\S+\.\S+/)
-    ]),
-    password: new FormControl("",[
-      Validators.required,
-    ])
-  });
-}
-
-  ngOnInit() {
-    // Condicionalidad de validadores según register
-    if (this.register) {
-      this.userForm.get('username')?.setValidators([Validators.required]);
-    } else {
-      this.userForm.get('username')?.clearValidators();
-    }
-    this.userForm.get('username')?.updateValueAndValidity(); //Actualiza las propiedades internas: valid, invalid, errors, status
+  constructor() {
+    this.userForm = new FormGroup({
+      username: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
 
   submitUser() {
     if (this.userForm.valid) {
-      console.log('submitted!')
+      console.log('submitted!');
       this.submitted.emit(this.userForm.value);
     } else {
       this.userForm.markAllAsTouched();
     }
   }
 
-   checkControl(controlName:string, errorName:string) : boolean  | undefined {
-    return this.userForm.get(controlName)?.hasError(errorName) 
-            && this.userForm.get(controlName)?.touched
+  checkControl(controlName: string, errorName: string): boolean | undefined {
+    return (
+      this.userForm.get(controlName)?.hasError(errorName) && this.userForm.get(controlName)?.touched
+    );
   }
-
 }
