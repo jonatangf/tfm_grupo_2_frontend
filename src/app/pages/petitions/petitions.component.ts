5da81fafb626267bcaeeb5560c059b9bb21315f7
Juan DELL <juancarrascoguerrero@gmail.com>
2025-11-27 13:17:36 +0100

    Integrando interfaz respuestas

--- src/app/pages/petitions/petitions.component.ts
+++ src/app/pages/petitions/petitions.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ITripResponse } from '../../interfaces/itrip.interface';
+import { TripsService } from '../../services/trips.service';
+import { ParticipationsService } from '../../services/participations.service';
+import { ITripJoinRequestResponse } from '../../interfaces/iparticipation.interface';
 
 @Component({
   selector: 'app-petitions',
@@ -11,7 +14,16 @@ export class PetitionsComponent {
 
   @Input() trip!: ITripResponse;
 
+  private tripsService = inject(TripsService);
+  private participationService = inject(ParticipationsService);
+
+  private joinPetitions! : ITripJoinRequestResponse[];
+
   subtitulo: string = 'Subtítulo dinámico'; // lo editas según tu lógica
+
+  async ngOnInit(){
+    this.joinPetitions = await this.participationService.getJoinRequests(this.trip.id); 
+  }
 
   solicitudes = [
     { user: { username: 'Carlos' }, expanded: false },
@@ -21,5 +33,14 @@ export class PetitionsComponent {
 
   toggleSolicitud(solicitud: any) {
     solicitud.expanded = !solicitud.expanded;
+  }
+
+  aceptarSolicitud(requestId:number){
+    console.log('aceptar');
+    console.log(requestId);
+  }
+
+  rechazarSolicitud(event:Event){
+    console.log(event);
   }
 }
