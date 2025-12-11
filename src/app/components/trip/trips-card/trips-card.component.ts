c1c3bf46591707d668734e9f254d97f4f2fff197
gentzane99 <gentzaneop@gmail.com>
2025-12-11 16:50:42 +0100

    añadida etiqueta de participacion
    fix en la primera y ultima card de trips que se ocultaba el borde

--- src/app/components/trip/trips-card/trips-card.component.ts
+++ src/app/components/trip/trips-card/trips-card.component.ts
@@ -1,9 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { ITripResponse } from '../../../interfaces/itrip.interface';
 import { TripListMode } from '../../../types/trip-types';
 import { DateRangePipe } from '../../../utils/date-format.pipe';
 import { CommonModule } from '@angular/common';
-
+import { ParticipationsService } from '../../../services/participations.service';
+import { UsersService } from '../../../services/users.service';
+import { ISession } from '../../../interfaces/users/isession';
 @Component({
   selector: 'app-trips-card',
   imports: [DateRangePipe, CommonModule],
@@ -19,7 +21,22 @@ export class TripCardComponent {
     @Output() detailClicked =  new EventEmitter<void>();
     @Output() requestClicked = new EventEmitter<ITripResponse>();
     @Output() editClicked = new EventEmitter<void>();
-    
+
+    userService = inject(UsersService);
+    participationService = inject(ParticipationsService);
+    isParticipant: boolean = false;
+
+    sesionData: ISession | null = {
+        userId: -1,
+        username: '',
+        email: '',
+        photo: '',
+    };
+
+    ngOnInit(){
+        this.participationStatus();
+    }
+
     joinPopUp(){
         this.joinClicked.emit();
     }
@@ -52,6 +69,28 @@ export class TripCardComponent {
                 return 'Cancelado';
             default:
                 return '';
+        }
+    }
+
+    participationLabel(): string | null {
+        if(!this.isParticipant) return null;
+        switch(this.trip.status){
+            case 'open': 
+            case 'closed':
+                return 'Participando';
+            case 'finished':
+                return 'Participaste';
+            case 'cancelled':
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    async participationStatus() {
+        const participants = await this.participationService.getTripMembers(this.trip.id);
+        if(participants){
+            this.isParticipant = participants.some( p => (p.userId === this.sesionData?.userId));
         }
     }
 }
